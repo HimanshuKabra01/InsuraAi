2fdbf0c0306fd71609ee89ece69b01f8eca7f4a2
vaibhav1010-geek <guptavaibhav0010@gmail.com>
2025-12-06 14:35:42 +0530

    fixed protect route

--- middleware/authMiddleware.js
+++ middleware/authMiddleware.js
@@ -1,23 +1,24 @@
 import jwt from "jsonwebtoken";
 import User from "../models/user.js";
 
-const authMiddleware = async (req, res, next) => {
-  let token;
+const protect = async (req, res, next) => {
+    let token;
 
-  if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
-    try {
-      token = req.headers.authorization.split(" ")[1];
-      const decoded = jwt.verify(token, process.env.JWT_SECRET);
-      const userId = decoded.id || decoded.sub; 
-      req.user = await User.findById(userId).select("-password");
-      next();
-      
-    } catch (err) {
-      console.error("JWT Verification failed:", err.message);
-      return res.status(401).json({ error: "Not authorized, token failed" });
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer")) {
+        try {
+            token = req.headers.authorization.split(" ")[1];
+            const decoded = jwt.verify(token, process.env.JWT_SECRET);
+            const userId = decoded.id || decoded.sub; 
+            req.user = await User.findById(userId).select("-password");
+            next();
+            
+        } catch (err) {
+            console.error("JWT Verification failed:", err.message);
+            return res.status(401).json({ error: "Not authorized, token failed" });
+        }
+    } else {
+        return res.status(401).json({ error: "Not authorized, no token" });
     }
-  } else {
-    return res.status(401).json({ error: "Not authorized, no token" });
-  }
 };
-export default authMiddleware;
+
+export { protect };
